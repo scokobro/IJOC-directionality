ed1720005d10300fcd6d2a46fb186b2ddf08d4e3
Scott Koga-Browes <spkb@Scott-MacBookPro.local>
2014-05-31 14:27:49 +0900

    comment out Oyama critique

---
+++ startMarkmon.sh
@@ -0,0 +1,1 @@
+cd /usr/local/bin; ./node /usr/local/bin/markmon "/Users/spkb/github/IJOC-directionality/IJOC-Direction - REVISED.gfm" --port 3333 --command "/usr/local/bin/pandoc --filter /usr/local/bin/pandoc-citeproc" --stylesheet "/Users/spkb/Desktop/GitHub2Mou.css"
